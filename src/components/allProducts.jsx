f4635338e33086317780764ac689a4b5316f9664
alejolac <alejobuc@gmail.com>
2023-11-01 13:45:21 -0300

    all products v:0.2

--- src/components/allProducts.jsx
+++ src/components/allProducts.jsx
@@ -1,15 +1,21 @@
 import Footer from "./footer.jsx"
 import NavBar from "./navBar.jsx"
 import Card from "./testCard.jsx"
+import data from "../assets/plants.jsx"
 
 const AllProduct = () => {
+    console.log(data[0].image[0]);
     return (
         <>
             <NavBar />
             <main>
                 <h1>Products Sempervirens</h1>
                 <div>
-
+                    {data.map((data) => (
+                        <div key={data.id}>
+                            <Card img={"../../public/"+data.image[0]} text={data.description} price={data.price} title={data.name}/>
+                        </div>
+                    ))}
                 </div>
             </main>
             <Footer />
--- src/components/testCard.jsx
+++ src/components/testCard.jsx
@@ -9,6 +9,7 @@ import imgImport from "../../public/img1.jpg"
 
 export default function ActionAreaCard({ img, title, text, price }) {
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    console.log(img, title, text, price);
     useEffect(() => {
         const handleResize = () => {
             setScreenWidth(window.innerWidth);
