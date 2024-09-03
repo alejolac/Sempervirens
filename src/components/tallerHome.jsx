33950e7fae50f16aa6d70bd3e70e89da9e1f34ee
Alejo <alejobuc@gmail.com>
2024-09-02 21:26:06 -0300

    Taller Home V:1.0

--- src/components/tallerHome.jsx
+++ src/components/tallerHome.jsx
@@ -6,39 +6,66 @@ const Blog = () => {
     return (
         <div className="container back-green my-5">
             <Title txt="Nuestro Taller" />
-            <div className="row justify-content-center">
+            <div className="row taller-home-head">
                 <div className="taller-home-item col-lg-5">
                     <div>
-                        <span>Un espacio para aprender</span>
+                        <span style={{
+                            fontSize: "36px",
+                            fontWeight: "500",
+                            color: "rgb(1 77 51)",
+                        }}>Un espacio para aprender</span>
                     </div>
-                    <div>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit necessitatibus, harum et error quo eum. Cum fugit maxime, ducimus temporibus
+                    <div style={{ color: "rgb(125, 125, 125)", lineHeight: "2" }}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit necessitati sectetur adipisicing elit. Odit necessitatibus,dolor, sit amet consectetur adipisi t consectetur adipisicing elit. Obus,dolor, sit amet consectetur adipisi t consectetur adipisicing elit. Odit necessitatibus,dolor, sit amet consectetur adipcing elit. Odit necessitatibu harum et error quo eum. Cum fugit maxime, ducimus temporibus
                     </div>
                     <div className="taller-home-info">
-                        <div>
+                        <span>
                             Mas Información
-                        </div>
-                        <div>
-                            <EastIcon color="success"/>
+                        </span>
+                        <div style={{ transition: "0.4s ease-in-out" }}>
+                            <EastIcon color="success" />
                         </div>
                     </div>
                 </div>
-                <div className="taller-home-item col-lg-5">
+                <div style={{gap: "0px"}} className="taller-home-item col-lg-5">
                     <div className="taller-home-elem">
-                       <div>
-                            1
-                       </div>
-                       <div>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus aspernatur velit
-                       </div>
+                        <div className="taller-home-index">
+                            01
+                        </div>
+                        <div className="taller-home-sec-head">
+                            <div className="taller-home-title-sec">
+                                Flores
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus aspernatur velit
+                            </div>
+                        </div>
                     </div>
                     <div className="taller-home-elem">
-                       <div>
-                            2
-                       </div>
-                       <div>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus aspernatur velit
-                       </div>
+                        <div className="taller-home-index">
+                            02
+                        </div>
+                        <div className="taller-home-sec-head">
+                            <div className="taller-home-title-sec">
+                                Árboles
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus aspernatur velit
+                            </div>
+                        </div>
+                    </div>
+                    <div className="taller-home-elem">
+                        <div className="taller-home-index">
+                            03
+                        </div>
+                        <div className="taller-home-sec-head">
+                            <div className="taller-home-title-sec">
+                                Semillas
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus aspernatur velit
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
