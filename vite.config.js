0c20a37ed4d28a781185ccab0f8bf7ae65861d90
Alejo <alejobuc@gmail.com>
2024-09-13 20:25:01 -0300

    vercel error v0.1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/sempervirens/",
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vercel() // Agrega el plugin de Vercel aquí
+  ],
+});
