8da8bd451fd4a8716b9b0a2d4eb1e88a9e16e933
Alejo <alejobuc@gmail.com>
2024-09-13 20:48:10 -0300

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/sempervirens/",
-  plugins: [
-    react(),
-    vercel() // Agrega el plugin de Vercel aquí
-  ],
-});
+  plugins: [react()],
+})
