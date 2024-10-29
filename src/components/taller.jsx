a71603d565eec0c46e88c89a9d04ba24d7079efc
Alejo <alejobuc@gmail.com>
2024-10-28 23:38:16 -0300

    npm update

--- src/components/taller.jsx
+++ src/components/taller.jsx
@@ -1,13 +1,32 @@
 import Footer from "./footer.jsx"
 import NavBar from "./navBar.jsx"
+import Title from "./titleSection.jsx"
 
 
 const Taller = () => {
     return (
         <>
-            <NavBar/>
-                <h1>Este es el taller de jardineria</h1>
-            <Footer/>        
+            <NavBar />
+            <main className="container contact-container">
+                <div className="back-green">
+                    <div className="mb-4">
+                        <Title txt="Talleres" />
+                        <div className="title-taller">
+                        <p className="contact-subtitle">Aprende y explora con nuestros talleres interactivos.</p> 
+                        </div>
+                    </div>
+                    <div className="taller-aside row">
+                        <div className="taller-aside-elem col-5">
+                            <h3>Jardineria</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quis nulla vero necessitatibus at odio</p>
+                        </div>
+                        <div className="taller-aside-elem col-5">
+                            <h3>Huerta</h3>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </>
     )
 }
