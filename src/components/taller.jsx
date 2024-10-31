0a1bfe31bd426893b5292f4ba21d98d4222189b0
Alejo <alejobuc@gmail.com>
2024-10-31 00:41:58 -0300

    taller program

--- src/components/taller.jsx
+++ src/components/taller.jsx
@@ -1,9 +1,25 @@
 import Footer from "./footer.jsx"
 import NavBar from "./navBar.jsx"
 import Title from "./titleSection.jsx"
+import Programa1 from "/programa.webp"
+
+import { useState } from "react"
 
 
 const Taller = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [imgModal, setImgModal] = useState()
+    window.scrollTo(0, 0);
+
+    const handleImageClick = (img) => {
+        setImgModal(img)
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
     return (
         <>
             <NavBar />
@@ -12,21 +28,38 @@ const Taller = () => {
                     <div className="mb-4">
                         <Title txt="Talleres" />
                         <div className="title-taller">
-                        <p className="contact-subtitle">Aprende y explora con nuestros talleres interactivos.</p> 
+                            <p className="contact-subtitle">Aprende y explora con nuestros talleres interactivos.</p>
                         </div>
                     </div>
                     <div className="taller-aside row">
                         <div className="taller-aside-elem col-5">
-                            <h3>Jardineria</h3>
+                            <div className="mb-3">
+                                <h3>Jardineria 🌱</h3>
+                            </div>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quis nulla vero necessitatibus at odio</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quis nulla vero necessitatibus at odio</p>
+                            <img onClick={() => handleImageClick(Programa1)} src={Programa1} alt="Programa Taller de Jardineria" />
                         </div>
                         <div className="taller-aside-elem col-5">
-                            <h3>Huerta</h3>
+                            <div className="mb-3">
+                                <h3>Huerta 🌱</h3>
+                            </div>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quis nulla vero necessitatibus at odio</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda quis nulla vero necessitatibus at odio</p>
+                            <img onClick={() => handleImageClick(Programa1)} src={Programa1} alt="Programa Taller de Jardineria" />
                         </div>
                     </div>
                 </div>
             </main>
             <Footer />
+            {modalOpen && (
+                <div className="modal-overlay" onClick={handleCloseModal}>
+                    <div className="modalIMG taller-modal">
+                        <img className="large-image-taller" src={imgModal} alt="" />
+                    </div>
+                    <span className="close-modal-x">&times;</span>
+                </div>
+            )}
         </>
     )
 }
