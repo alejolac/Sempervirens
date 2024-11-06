6611630abc0521dbdf8cfb7c50cf1103fbb75e35
Alejo <alejobuc@gmail.com>
2024-11-06 00:14:41 -0300

    newBlog

--- src/assets/blog.jsx
+++ src/assets/blog.jsx
@@ -7,24 +7,28 @@ const data = [
         title: "Lapachos 🌳",
         description: "Si aprecias a la distancia manchas rosadas en el paisaje son ellos, sin duda, los LAPACHOS",
         content: `
-            <p>Si aprecias a la distancia manchas rosadas en el paisaje son ellos, sin duda, los LAPACHOS (Handroanthus impetiginosus)!</p>
-            <img class="mb-3" src="/blogImage1.jpg" alt="Placeholder Image" width="600" height="400"/>
-            <p>árbol nativo que por estos días nos cautiva con su abundante floración. En Bvar Artigas desde el nuevo centro hacia el Prado los podrás apreciar, ya que hay toda una alineación de ellos.
-            Esperemos comenzar a verlo por muchos lugares más!!🌸🌿</p>
+        <p>Si aprecias a la distancia manchas rosadas en el paisaje son ellos, sin duda, los LAPACHOS (Handroanthus impetiginosus)!</p>
+        <img class="responsive-img mb-3" src="/blogImage1.jpg" alt="Lapacho en flor" />
+        <p>Árbol nativo que por estos días nos cautiva con su abundante floración.</p> 
+        <p> En Bvar Artigas desde el nuevo centro hacia el Prado los podrás apreciar, ya que hay toda una alineación de ellos.
+        ¡Esperemos comenzar a verlo por muchos lugares más!🌸🌿</p>
         `,
         date: "31-10-2024",
         parseDate: "2024-10-31"
     },
     {
         id: 2,
-        imgPortada: "/blogImage1.jpg",
-        title: "Lapachos 🌳",
-        description: "Si aprecias a la distancia manchas rosadas en el paisaje son ellos, sin duda, los LAPACHOS",
+        imgPortada: "/blogImage3.jpg",
+        title: "Suculentas 🌸",
+        description: "Las suculentas no escapan a la primavera … sino miren estos granitos de arroz a pura floración.",
         content: `
-            <p>Si aprecias a la distancia manchas rosadas en el paisaje son ellos, sin duda, los LAPACHOS (Handroanthus impetiginosus)!</p>
-            <img class="mb-3" src="/blogImage1.jpg" alt="Placeholder Image" width="600" height="400"/>
-            <p>árbol nativo que por estos días nos cautiva con su abundante floración. En Bvar Artigas desde el nuevo centro hacia el Prado los podrás apreciar, ya que hay toda una alineación de ellos.
-            Esperemos comenzar a verlo por muchos lugares más!!🌸🌿</p>
+            <p>🌸Las suculentas no escapan a la primavera … sino miren estos granitos de arroz a pura floración.
+            Su nombre común se debe a la forma de sus pequeñas hojas como granitos de arroz.
+            Como toda suculenta es mínimo su riego y, en este caso, tiene la particularidad de ser una planta sumamente colgante o también como tapizante del suelo.
+            Una belleza verdad?? 🌸🌱🌱</p>
+            <div>
+                <img class="mb-3" src="/blogImage3.jpg" alt="Placeholder Image" />
+            </div>
         `,
         date: "31-10-2024",
         parseDate: "2024-10-31"
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
                                 <h5 className="mt-5">Bienvenidos a Sempervirens</h5>
                                 <h1 className="mt-2">¡Haz crecer un jardín, haz <br /> crecer tus sueños!</h1>
                                 <p className="my-4">
-                                    Descubre la magia de la naturaleza y aprende a cultivar tu propio oasis. En Sempervirens, te brindamos el conocimiento y las herramientas para hacer florecer cada rincón de tu espacio. ¡Sumérgete en el mundo de las plantas y transforma tu entorno!
+                                Descubre la magia de la naturaleza y aprende a crear tu propio oasis. En Sempervirens, te damos las herramientas para transformar cada rincón de tu espacio.
                                 </p>
                                 <Link to={"/contacto"}>
                                     <Button sx={{ alignSelf: "flex-start", backgroundColor: "green" }} className="btn-aplicacion" variant="contained">Contactenos</Button>
